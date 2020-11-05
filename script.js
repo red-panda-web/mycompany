e6ec2d552fa7325e3600f086eee295d833ae8c03
red-panda-web <readex934@gmail.com>
2020-11-05 22:26:27 +0300

    added fifth sections

--- script.js
+++ script.js
@@ -7,5 +7,16 @@ document.addEventListener("DOMContentLoaded", function () {
 	document.querySelector(".header-body__burger").addEventListener("click", function () {
 		document.querySelector(".header-body__burger").classList.toggle("active");
 		document.querySelector(".burger-menu").classList.toggle("active");
+	});
+
+	let slides_count = Math.floor(document.querySelector(".swiper-clients").clientWidth / 200);
+	var Swiper1 = new Swiper('.swiper-clients', {
+		loop: true,
+		slidesPerView: slides_count,
+		// Navigation arrows
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
 	})
 });
