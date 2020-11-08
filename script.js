d6389860943b7861ec28bbc016ce7a8efdb12150
red-panda-web <readex934@gmail.com>
2020-11-08 21:06:31 +0300

    added sixth && seventh sections

--- script.js
+++ script.js
@@ -9,14 +9,26 @@ document.addEventListener("DOMContentLoaded", function () {
 		document.querySelector(".burger-menu").classList.toggle("active");
 	});
 
-	let slides_count = Math.floor(document.querySelector(".swiper-clients").clientWidth / 200);
-	var Swiper1 = new Swiper('.swiper-clients', {
+	// clients slider
+	let slides_count_swiper1 = Math.floor(document.querySelector(".swiper-clients").clientWidth / 230);
+	var Swiper1 = new Swiper('.swiper-container.swiper-clients', {
 		loop: true,
-		slidesPerView: slides_count,
-		// Navigation arrows
+		slidesPerView: slides_count_swiper1,
 		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
+			nextEl: '.swiper-button-next.swiper-clients__arrow-r',
+			prevEl: '.swiper-button-prev.swiper-clients__arrow-l',
 		},
-	})
+	});
+
+	//our-works slider
+	let slides_count_swiper2 = Math.floor(document.querySelector(".swiper-work").clientWidth / 350);
+	var Swiper2 = new Swiper('.swiper-container.swiper-work', {
+		loop: true,
+		slidesPerView: slides_count_swiper2,
+		navigation: {
+			nextEl: '.swiper-button-next.swiper-work__arrow-r',
+			prevEl: '.swiper-button-prev.swiper-work__arrow-l',
+		},
+	});
+
 });
